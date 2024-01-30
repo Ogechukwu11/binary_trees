78210805092087b91e5bb7389058f1dcbdf67ea0
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-01-30 06:33:54 +0000

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,7 +14,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (newnode == NULL)
 		return (NULL);
 
-	newnode->n = value;
 	newnode->parent = parent;
+	newnode->n = value;
+
 	return (newnode);
 }
